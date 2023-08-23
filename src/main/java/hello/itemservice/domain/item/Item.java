ca130e7c90a4b32a4e5812a56c5aaa5e740393c1
Citrons06 <yeolee1212@gmail.com>
2023-08-23 19:24:08 +0900

    study0823

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -7,8 +7,8 @@ public class Item {
 
     private Long id;
     private String itemName;
-    private Integer price;
-    private Integer quantity;
+    private Integer price;      // price가 null인 경우도 있다고 가정
+    private Integer quantity;   // 수량이 null인 경우도 있다고 가정
 
     public Item() {
     }
